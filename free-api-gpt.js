b0cd4bfbc2b0c4262af46531556f969aed6840db
Ciptik <50663005+Ciptik@users.noreply.github.com>
2025-03-01 16:28:59 +0300

    Update free-api-gpt.js

--- free-api-gpt.js
+++ free-api-gpt.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2024 Ciptik
+ * Copyright (c) 2025 Ciptik
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
